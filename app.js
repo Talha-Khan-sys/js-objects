0134a3e5782098c46e48c28f5b3b6898abcbe2f2
Muhammad Talha Khan <hdmoviez1234@gmail.com>
2021-12-10 21:53:27 +0500

    js object

---
+++ app.js
@@ -0,0 +1,25 @@
+// var obj = {
+// //         name : "Talha",
+// //         age : 18,
+// //         profession : "student",
+// // }
+// // obj.sudy = "University";
+
+// // console.log(obj.name);
+// // console.log(obj.age);
+// // console.log(obj.sudy);
+// // console.log(obj.profession);
+
+
+// function Student(name,age,school){
+//         this.name = name;
+//         this.age = age;
+//         this.school = school;
+
+// }                
+// var student = new Student('Talha',18,'Playway Grammar School');                      
+// var student2 = new Student('Yousaf', 13, 'Playway Grammar School');                      
+// var student3 = new Student('Taha',14, 'Playway Grammar School');
+// console.log(student);
+// console.log(student2);
+// console.log(student3);
